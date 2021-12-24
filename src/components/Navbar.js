e19f037f9eb465c5aa5587c9ecd05c031a849948
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-24 20:14:03 +0100

    First try to make navbar responsive: Failed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,12 @@ import { ArrowRightIcon } from '@heroicons/react/solid'
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
-        <a className="flex flex-wrap items-center text-base justify-center">
-          <a href="#about" className="ml-3 text-xl">
-            <img src="./M.png" className="w-10" alt="M logo"></img>
-          </a>
+      <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+        <a
+          href="#about"
+          className="flex flex-wrap items-center text-base justify-center ml-3 text-xl"
+        >
+          <img src="./M.png" className="w-8" alt="M logo"></img>
         </a>
         <div className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
@@ -26,7 +27,7 @@ const Navbar = () => {
         </div>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-red-900 rounded text-base mt-4 md:mt-0"
         >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
