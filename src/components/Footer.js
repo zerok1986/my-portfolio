2e94e765d35ea4b23b86ef3245da891588a98856
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-12-26 23:46:35 +0100

    [FIX]: Footer text size in responsive + [FIX]: Color of the mobile-menu button not visible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-gray-800 relative inset-x-0 bottom-0 h-8">
       <div className="flex flex-row justify-center content-center">
         <CodeIcon className="text-red-800 w-5 h-5 ml-2 mr-2" />
-        <p>
+        <p className="text-xs sm:text-base">
           Developed by <b> Miguel Angel Abad</b>
         </p>
         <div className="flex flex-row justify-center content-center ml-5">
@@ -48,7 +48,7 @@ const Footer = () => {
               <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
             </svg>
           </a>
-          ©️ 2021
+          <span className="text-xs sm:text-base">2021</span>
         </div>
       </div>
     </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             <button
               onClick={() => setIsOpen(!isOpen)}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-900"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
@@ -34,7 +34,7 @@ const Navbar = () => {
                 </svg>
               ) : (
                 <svg
-                  className="hidden h-6 w-6"
+                  className="block h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
