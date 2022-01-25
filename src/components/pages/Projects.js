6bda0a1348c38cb329263d23f20e0334ea203b50
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-25 22:27:16 +0100

    (Fix): Same height of Project box containers

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -28,15 +28,15 @@ const Projects = () => {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 min-h-fit p-4"
+              className="sm:w-1/2 w-100 min-h-full p-4"
             >
-              <div className="flex relative">
+              <div className="flex relative h-64">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-100 dark:bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative align-middle z-10 w-full border-4 border-gray-800 bg-gray-100 dark:bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-red-700 mb-1">
                     {project.subtitle}
                   </h2>
